722d03a7ca74ccf897f1f752d0d9daa2a9c5a34d
akinolapo <akinolapo@gmail.com>
2025-11-21 21:33:16 +0100

    all components

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,14 +1,15 @@
-import AnimatedCounter from './AnimatedCounter'
-import DoughnutChart from './DoughnutChart'
+import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({
-  accounts= [], totalBanks, totalCurrentBalance
-}:TotlaBalanceBoxProps) => {
+  accounts = [], totalBanks, totalCurrentBalance
+}: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
-        <DoughnutChart accounts={accounts}/>
+        <DoughnutChart accounts={accounts} />
       </div>
+
       <div className="flex flex-col gap-6">
         <h2 className="header-2">
           Bank Accounts: {totalBanks}
@@ -17,8 +18,9 @@ const TotalBalanceBox = ({
           <p className="total-balance-label">
             Total Current Balance
           </p>
+
           <div className="total-balance-amount flex-center gap-2">
-            <AnimatedCounter amount= {totalCurrentBalance}/>
+            <AnimatedCounter amount={totalCurrentBalance} />
           </div>
         </div>
       </div>
